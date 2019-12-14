f6ecbc2ad99d22c5168a275ee1efd56fb5bc1ac5
Borey <skborey@gmail.com>
2019-12-14 23:21:04 +0700

    seperate collection menu action

--- frontend/src/components/collectionMenu.js
+++ frontend/src/components/collectionMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { addNewCollection, deleteCollection, showRestaurantInCollection } from '../actions';
+import { addNewCollection, deleteCollection, showRestaurantInCollection } from '../actions/collectionMenuAction';
 
 class CollectionMenu extends Component {
 
