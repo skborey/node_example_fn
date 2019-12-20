f05c15f29bfa060cdd03ece33df909ad5fec18e3
Borey <skborey@gmail.com>
2019-12-21 02:24:27 +0700

    make collaborator show base on the collaborator in selected list

--- src/components/CollaboratorList.js
+++ src/components/CollaboratorList.js
@@ -8,17 +8,49 @@ class CollaboratorList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            selectedRestaurant: {},
-            selectedCollection: {}
-        }
+        this.state = {}
+    }
+
+    handleFinishRename = (e) => {
+        let id = e.target.id;
+        let name = e.target.innerText
+        console.log(id, name);
+    }
+
+    handleOnDeleteCollaborator = (id) => {
+        console.log(id);
     }
 
     render () {
+
+        let collections = this.props.collectionList.collections;
+        let collaborators = this.props.collectionList.collaborators;
+        let collaboratorMenu = (this.props.showCollectionId) ?
+            collections[this.props.showCollectionId].collaborators.map((id, index) => {
+                return (
+                    <li key={index} >
+                        <span
+                            id={collaborators[id]._id}
+                            title='Click to rename'
+                            contentEditable="true"
+                            onBlur={this.handleFinishRename}
+                        >
+                            {collaborators[id].name}
+                        </span>
+                        <span 
+                            title='Delete collection' 
+                            onClick={() => this.handleOnDeleteCollaborator(collaborators[id]._id)}
+                        >x</span>
+                    </li>
+                )}) : null;
+
         return (
             (this.props.sessions.email) ? (
                 <div className="collaborator-menu-cls">
                     <h5>Collaborators</h5>
+                    <ul>
+                        {collaboratorMenu}
+                    </ul>
                     <button className="btn-addnew-cls">+ Add New Collaborator</button>
                 </div>
             ):(null)
@@ -28,7 +60,9 @@ class CollaboratorList extends Component {
 
 export default connect(
   (state, props) => ({
-    sessions: state.sessions
+    sessions: state.sessions,
+    collectionList: state.collectionList,
+    showCollectionId: state.showCollectionId,
   }),
   {
   }
